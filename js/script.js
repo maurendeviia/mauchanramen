213d482b8b86c159c9ec50486742388557149854
Mauren Helvia Devi <maurendeviia@gmail.com>
2022-08-01 22:04:43 +0700

    change directory

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function allmenu(){
-    $.getJSON('http://localhost/ramen/data/ramen.json ', function (data) {
+    $.getJSON('data/ramen.json ', function (data) {
         let menu = data.menu;
         $.each(menu, function (i, data) {
             $('#menu_ramen').append('<div class="col-md-4"><div class="card mb-3"><img src=' + data.gambar + ' class="card-img-top"><div class="card-body"><h5 class="card-title">' + data.nama + '</h5><p class="card-text">' + data.deskripsi + '</p><h5 class="card-title">Rp.' + data.harga + ',-</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a></div></div></div>');
@@ -18,7 +18,7 @@ $('.nav-link').on('click', function () {
         return;
     }
 
-    $.getJSON('http://localhost/ramen/data/ramen.json ', function (data) {
+    $.getJSON('data/ramen.json ', function (data) {
         let menu = data.menu;
         let content = '';
 
